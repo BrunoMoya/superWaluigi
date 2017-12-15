781951f39923e0a0c56527d0229c1bdcc5418074
Marcos Llinares <marcolli@ucm.es>
2017-12-15 16:20:14 +0100

    Tilemap y animaciones

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,6 @@ var BootScene = {
   },
 
   create: function () {
-
     this.game.state.start('preloader');
   }
 };
@@ -22,11 +21,14 @@ var PreloaderScene = {
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.image('waluigi', 'images/whaaaa.png');
+    //this.game.load.image('waluigi', 'images/whaaaa.png');
+    this.game.load.spritesheet ('wa2','images/Waluigisheet15-12.png', 56 , 88);
     this.game.load.image('ground', 'images/platform.png');
     this.game.load.image('goomba', 'images/goombat.png');
     this.game.load.image('wall', 'images/wall.png');
-
+    //
+    this.game.load.spritesheet('patronesTilemap', 'images/super_mario.png',16 ,16);
+    this.game.load.tilemap('tilemap', 'images/tilemapW15-12.csv', null, Phaser.Tilemap.TILED_JSON);//Phaser.Tilemap.CSV , usar en el 4º argumento si no funciona
   },
 
   create: function () {
--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,106 +1,108 @@
 'use strict';
 
-var waluigi;
+var layer5;
+var layer4;
+var layer3;
+var bloques;
+var layer;
+var wa2;
 var goomba;
 var platforms;
 var cursors;
 var canJump;
+var suelo;
+var bloques;
 
 var PlayScene = {
   create: function () {
 
+  this.map = this.game.add.tilemap('tilemap');
+  this.map.addTilesetImage("1","patronesTilemap");
+  layer4 = this.map.createLayer('cielo');
+    //
+  wa2 = this.game.add.sprite( this.game.world.centerX, this.game.world.centerY,'wa2');
+  wa2.anchor.setTo(0.5, 0.5);
   this.game.physics.startSystem(Phaser.Physics.ARCADE);
+  this.game.physics.arcade.enable(wa2);
+  wa2.body.collideWorldBounds = true;
+  wa2.animations.add('walkRight', [0, 1, 2, 3, 4], 10, true);
+  wa2.animations.add('walkLeft', [6, 7, 8, 9, 10], 10, true);
+  wa2.animations.add('mecachis',[18, 19, 20],7,true);
+  wa2.animations.add('tomaBola',[21, 22, 23],4,true);
+  wa2.animations.add('entrando',[12, 13, 14,15,16],4,true);
+  wa2.goesRight = true;
 
-    platforms = this.game.add.group();
-    platforms.enableBody = true;
+  ////////////////
+  
 
-    var ground = platforms.create(0, this.game.world.height - 64, 'ground');
+  
+  this.layer3 = this.map.createLayer('?');
+  this.bloques = this.map.createLayer('bloques');
+  this.suelo = this.map.createLayer('suelo');
+  this.layer5 = this.map.createLayer('bandera');
+  this.suelo.resizeWorld();
+  //bloques.resizeWorld();
+  //layer3.resizeWorld();
+  //layer4.resizeWorld();
+  //suelo.debug = true;
+  
+    this.game.physics.enable(wa2, Phaser.Physics.ARCADE);
 
-   ground.scale.setTo(2, 2);
-
-   ground.body.immovable = true;
-
-   var ledge = platforms.create(400, 400, 'ground');
-
-   ledge.body.immovable = true;
-
-   ledge = platforms.create(-150, 250, 'ground');
-
-   ledge.body.immovable = true;
-
-   var wall = platforms.create(-10, 300, 'wall');
-
-   wall.body.immovable = true;
-
-    waluigi = this.game.add.sprite(
-      this.game.world.centerX, this.game.world.centerY, 'waluigi');
-    waluigi.anchor.setTo(0.5, 0.5);
-
-    goomba = this.game.add.sprite(
-      750, this.game.world.centerY, 'goomba');
-    goomba.anchor.setTo(0.5, 0.5);
-
-    this.game.physics.enable(waluigi, Phaser.Physics.ARCADE);
-    this.game.physics.enable(goomba, Phaser.Physics.ARCADE);
-
-    waluigi.body.collideWorldBounds = true;
-    waluigi.body.gravity.y = 500;
-    waluigi.body.bounce.y = 0.2;
-
-    goomba.body.gravity.y = 500;
-    goomba.body.bounce.y = 0.2;
-
+    wa2.body.collideWorldBounds = true;
+    wa2.body.gravity.y = 370;
+    wa2.body.bounce.y = 0.2;
+    this.map.setCollisionBetween(13, 15, true, 'suelo');
+    //this.map.setCollision(39);
+   // this.map.setCollision(true, 'suelo');
     cursors = this.game.input.keyboard.createCursorKeys();
     canJump = true;
+    this.game.camera.follow(wa2);
 
   },
 
   update:  function () {
 
-    this.game.physics.arcade.collide(waluigi, platforms);
-    this.game.physics.arcade.collide(goomba, platforms, function(goomba, platforms){
+    this.game.physics.arcade.collide(wa2, suelo);
+    this.game.physics.arcade.collide(wa2, bloques);
+    this.map.setCollision(true, 'suelo');
 
-      if(goomba.body.touching.right && goomba.body.touching.down)
-        goomba.scale.x = 1;
-
-      if(goomba.body.touching.left && goomba.body.touching.down)
-        goomba.scale.x = -1;
-    }, null, this);
-
-      goomba.body.velocity.x = -100 * goomba.scale.x;
-
-    this.game.physics.arcade.collide(waluigi, goomba, function(waluigi, enemy){
-
-      if(goomba.body.touching.up && waluigi.body.touching.down){
-            goomba.kill();
-        }
-        else  this.game.state.start('play');
-      }, null, this);
-
-    if(waluigi.body.touching.down) canJump = true;
-
-    if (cursors.left.isDown && waluigi.body.velocity.x > -100){
-      if(waluigi.body.velocity.x > 0)
-        waluigi.body.velocity.x /= 1.1;
-      waluigi.body.velocity.x -= 10;
+      if (wa2.body.enable) {
+    wa2.body.velocity.x = 0;
+    if (cursors.left.isDown) {
+      wa2.body.velocity.x = -90;
+      wa2.animations.play('walkLeft');
+      wa2.goesRight = false;
+    } else if (cursors.right.isDown) {
+      wa2.body.velocity.x = 90;
+      wa2.animations.play('walkRight');
+      wa2.goesRight = true;
+    } else if(cursors.down.isDown){
+       wa2.animations.play('entrando');
+    /*
+      wa2.animations.stop();
+      if (wa2.goesRight) wa2.frame = 0;
+      else wa2.frame = 7;*/
     }
-    else if (cursors.right.isDown && waluigi.body.velocity.x < 100){
-      if(waluigi.body.velocity.x < 0)
-        waluigi.body.velocity.x /= 1.1;
-      waluigi.body.velocity.x += 10;
-    }
-    else if (!cursors.right.isDown && !cursors.left.isDown){
-        waluigi.body.velocity.x /= 1.1;
-    }
-    if (cursors.up.isDown && canJump){
-      if(waluigi.body.velocity.y < -300) canJump = false;
-      waluigi.body.velocity.y -= 20;
-    }
+/*
+    if (cursors.up.isDown && wa2.body.onFloor()) {
+      wa2.body.velocity.y = -190;
+      wa2.animations.stop();
+    }*/
+    
+/*  //salto
+    if (wa2.body.velocity.y != 0) {
+      if (wa2.goesRight) wa2.frame = 5;
+      else wa2.frame = 12;
+    }*/
+  }
+
   },
 
  render: function () {
-       this.game.debug.bodyInfo(waluigi, 32, 32);
-       this.game.debug.body(waluigi);
+       //this.game.debug.bodyInfo(waluigi, 32, 32);
+       //this.game.debug.body(waluigi);
+       this.game.debug.bodyInfo(wa2, 20, 20);
+       this.game.debug.body(wa2);
      }
    };
 
