67bd22cbbccf0a7c562480c38886d8a8946e3c21
unknown <brunomoy@ucm.es>
2017-12-19 13:51:28 +0100

    errores hito2

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -82,12 +82,12 @@ var PreloaderScene = {
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.tilemap('tilemapWa', 'images/planoFisico8.json', null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('super_mario', 'images/super_mario.png',16,16 );
-    this.game.load.spritesheet('waluigi', 'images/Waluigisheet15-12.png',56 ,88);
-    this.game.load.image('ground', 'images/platform.png');
-    this.game.load.image('goomba', 'images/goombat.png');
-    this.game.load.image('wall', 'images/wall.png');
+    this.game.load.tilemap('tilemapWa', '../images/planoFisico8.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image('super_mario', '../images/super_mario.png',16,16 );
+    this.game.load.spritesheet('waluigi', '../images/Waluigisheet15-12.png',56 ,88);
+    this.game.load.image('ground', '../images/platform.png');
+    this.game.load.image('goomba', '../images/goombat.png');
+    this.game.load.image('wall', '../images/wall.png');
 
   },
 
